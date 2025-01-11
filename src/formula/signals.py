343f309a690c7412fda04b4d6eb88acabba5fdc6
lukas <lukasvinclav@gmail.com>
2025-01-11 19:14:40 +0100

    feat: prevent modifications

--- src/formula/signals.py
+++ src/formula/signals.py
@@ -1,13 +1,22 @@
 from django.conf import settings
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from formula.exceptions import ReadonlyException
 
 
-@receiver(pre_save)
-def update_timestamp(sender, instance, **kwargs):
+def prevent_modifications(sender, instance, **kwargs):
     if not settings.DEBUG and sender._meta.db_table != "studio_options":
         raise ReadonlyException(
             "Database is operating in readonly mode. Not possible to save any data."
         )
+
+
+@receiver(pre_save)
+def block_save(sender, instance, **kwargs):
+    prevent_modifications(sender, instance, **kwargs)
+
+
+@receiver(pre_delete)
+def block_delete(sender, instance, **kwargs):
+    prevent_modifications(sender, instance, **kwargs)
